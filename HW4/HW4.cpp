8b67c33759e63c304d6363a80b65134cdb381084
Delina <delinayemane2004@gmail.com>
2024-09-14 18:47:35 -0700

    Add project files.

---
+++ HW4/HW4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Course     | Student \n";
+    cout << "\n";
+
+    cout << "C++           100\n";
+    cout << "JavaScript     50\n";
+
+    return 0;
+}
+
